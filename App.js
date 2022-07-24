d6c0a92e5701164a0f8ece8f0397cbcd531fbf27
adyhatem65 <adyetman65@gmail.com>
2022-07-24 10:24:22 +0200

    Added all folders

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React, {Component} from 'react';
 
 import {COLORS} from './src/constants';
@@ -6,11 +6,18 @@ import {COLORS} from './src/constants';
 class App extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>App</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.error,
+    flex: 1,
+  },
+});
 
 export default App;
