02395d6e105946773159faea894bc4a6af208add
abdllrhmanelshazly <abdllrhmanelshazly@gmial.com>
2022-09-14 03:06:14 +0200

    added update

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 // native base
-import {NativeBaseProvider} from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // navigation
 import ContainerNavigation from './src/navigation';
