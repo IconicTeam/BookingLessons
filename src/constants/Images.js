ba23edf1e151182bfcc3aeab036ae81d9a9ec1fe
Youssef Mohamed <yo.mohamed2001@gmail.com>
2022-09-21 03:10:12 +0200

    added in resevation page

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -2,10 +2,12 @@ const login = require('../assets/Images/login.png');
 const confirm = require('../assets/Images/confirm.png');
 const otp = require('../assets/Images/otp.png');
 const phone = require('../assets/Images/phone.png');
+const students=require('../assets/Images/students.png')
 
 export default {
   login,
   confirm,
   otp,
   phone,
+  students
 };
--- src/screens/home/Reservation.js
+++ src/screens/home/Reservation.js
@@ -1,14 +1,104 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import {
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Keyboard,
+  Pressable,
+} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-const Reservation = () => {
+import components from '../../components';
+import { textStyles } from '../../styles';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import OTPInput from '@hirbod/react-native-otp-input';
+import { COLORS, FONTS, Images, PADDINGS, RADIUS } from '../../constants';
+const { width, height } = Dimensions.get('screen');
+export default class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render(){
   return (
-    <View>
-      <Text>Reservation</Text>
-    </View>
+    <>
+      <components.Container>
+        <components.GeneralStatusBar />
+        <components.MainHeader
+          // navigation={this.props.navigation}
+          title={"قائمة الحجوزات"}
+          haveBackButton={true}
+        />
+        <ScrollView>
+          <View style={styles.mainView}>
+            <TouchableOpacity
+            activeOpacity={0.4}
+            
+            style={styles.button}>
+              <Image source={Images.students}
+               style={styles.image}
+              />
+              <Text style={textStyles.lgTextStyle}>الصف الاول</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+            <Image source={Images.students}
+               style={styles.image}
+              />
+              <Text style={textStyles.lgTextStyle}>الصف الثاني</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+             <Image source={Images.students}
+               style={styles.image}
+              />
+              <Text style={textStyles.lgTextStyle}>الصف الثالث</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{padding:PADDINGS.mdPadding+10}}> 
+        
+            </View>
+          
+        </ScrollView>
+      </components.Container>
+    </>
   );
-};
+  }
+}
 
-export default Reservation;
 
-const styles = StyleSheet.create({});
+
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection:"row",
+    justifyContent:"space-between",
+    flexWrap:"wrap",
+    padding:PADDINGS.mdPadding
+  },
+  button: {
+    width: "47%",
+    height: RFValue(200),
+    backgroundColor:"#f4f4f4",
+    marginBottom:20,
+    borderRadius: RADIUS.mdRadius,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation:2
+
+  },
+  image:{
+    width: RFValue(120),
+    height: RFValue(120),
+    marginBottom:5,
+    borderRadius:RADIUS.mdRadius,
+    resizeMode: 'contain',
+  }
+});
